ec0920338dd0cf24e43c387fba79b39c69baf371
kumarichaa <richakumari23031@gmail.com>
2026-01-06 11:44:36 +0530

    logic building with c

--- C_Programming/Assignments/Assignment_c12.c
+++ C_Programming/Assignments/Assignment_c12.c
@@ -18,20 +18,167 @@ WAP to print the table of N.
 #include <stdio.h>
 
 // WAP to print "MySirG" N times on the screen
+
+// int main()
+// {
+//     int Nth_term;
+//     printf("Please Nth term\n");
+//     scanf("%d", &Nth_term);
+
+//     for (int i = 1; i <= Nth_term; i++)
+//     {
+//         printf("\"MySirG\"\n");
+//     }
+// }
+
+// WAP to print the first N natural numbers.
+
+// int main()
+// {
+//     int Nth_term;
+//     printf("Please Nth term\n");
+//     scanf("%d", &Nth_term);
+
+//     for (int i = 1; i <= Nth_term; i++)
+//     {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// WAP to print the first N natural numbers in reverse order.
+
+// int main()
+// {
+//     int Nth_term;
+//     printf("Please Nth term\n");
+//     scanf("%d", &Nth_term);
+
+//     for (int i = Nth_term; i >= 1; i--)
+//     {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// WAP to print the first N odd natural numbers.
+
+// int main()
+// {
+// int Nth_term;
+// printf("Please Nth term\n");
+//     scanf("%d", &Nth_term);
+
+//     for(int i=1;i<=Nth_term;i++){
+//         if(i%2){
+//         printf("%d ",i);
+//         }
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// WAP to print the first N odd natural numbers in reverse order.
+
+// int main()
+// {
+//     int Nth_term;
+//     printf("Please Nth term\n");
+//     scanf("%d", &Nth_term);
+
+//     for (int i = Nth_term; i >= 1; i--)
+//     {
+//         if (i % 2)
+//         {
+//             printf("%d ", i);
+//         }
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// WAP to print the first N even natural numbers.
+
+// int main()
+// {
+//     int Nth_term;
+//     printf("Please Nth term\n");
+//     scanf("%d", &Nth_term);
+
+//     for (int i = 1; i <= Nth_term; i++)
+//     {
+
+//         printf("%d ", 2 * i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// WAP to print the first N even natural numbers in reverse order.
+
+// int main()
+// {
+//     int Nth_term;
+//     printf("Please Nth term\n");
+//     scanf("%d", &Nth_term);
+
+//     for (int i = Nth_term; i >= 1; i--)
+//     {
+
+//         printf("%d ", 2 * i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// WAP to print  squares of the first N natural numbers.
+
+// int main()
+// {
+//     int Nth_term;
+//     printf("Please Nth term\n");
+//     scanf("%d", &Nth_term);
+
+//     for (int i = 1; i <= Nth_term; i++)
+//     {
+
+//         printf("%d ", i * i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// WAP to print cubes of the first N natural numbers.
+
 int main()
 {
+    int Nth_term;
+    printf("Please Nth term\n");
+    scanf("%d", &Nth_term);
 
-    int num1;
-    printf("Please enter a number\n");
-    scanf("%d", &num1);
-    if (num1 %= 2)
+    for (int i = 1; i <= Nth_term; i++)
     {
-        printf("Given number is an odd\n");
-        return 0;
+
+        printf("%d ", i * i * i);
     }
-    else
-    {
-        printf("Given number is an even\n");
-        return 0;
-    }
+    printf("\n");
+    return 0;
 }
+
+// WAP to print the table of N.
+
+// int main()
+// {
+//     int Nth_term;
+//     printf("Please enter Nth term for table\n");
+//     scanf("%d", &Nth_term);
+
+//     for (int i = 1; i <= 10; i++)
+//     {
+
+//         printf(" %d * %d = %d \n", Nth_term, i, Nth_term * i);
+//     }
+//     return 0;
+// }
